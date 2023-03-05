c1ca8c236b6d8a7085597cdead18fa0e1ca9b5b0
sha2nkprasad <sha2nkprasad@gmail.com>
2023-03-06 00:29:31 +0530

    graphql codegen setup

--- src/components/CharacterList/index.tsx
+++ src/components/CharacterList/index.tsx
@@ -1,9 +1,17 @@
 import { Avatar, Grid, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
 import React from "react";
+import { useGetCharactersQuery } from "../../generated/graphql";
 
 const items = [1,2,3,4];
 
 const CharacterList = () => {
+
+    const { data, loading } = useGetCharactersQuery({
+        fetchPolicy: 'network-only',
+    });
+    console.log('----loading :', loading);
+    console.log('----data :', data);
+
     return (
         <Grid container>
             <Grid item xs={12} md={6}>
